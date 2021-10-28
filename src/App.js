ced1df22943c6c04b0829796b21318342af408fd
christiancifuentes <christian_cifuentes@epam.com>
2021-10-28 16:03:13 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Hearder from './components/Header/Header';
+import Courses from './components/Courses/components/Courses/Courses';
+import CreateCourse from './components/CreateCourse/CreateCourse';
+import { useState } from 'react';
+const appStyle = {
+	margin: '1% 1% 0% 1%',
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+	const [state, setState] = useState('');
+	return (
+		<div style={appStyle}>
+			<Hearder />
+			{state === 'false' || state === 'undefiend' ? (
+				<CreateCourse stateChanger={setState} />
+			) : (
+				<Courses stateChanger={setState} />
+			)}
+		</div>
+	);
+};
 export default App;
